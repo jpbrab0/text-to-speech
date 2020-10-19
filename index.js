623b8b215bcce19395be408303b803aff02c1aac
João Pedro <jpresende502@gmail.com>
2020-10-19 19:14:02 -0300

    Adicionando arquivos

---
+++ index.js
@@ -0,0 +1,25 @@
+// - Jquery + Js Vannila
+const utterance = new SpeechSynthesisUtterance();
+
+utterance.lang = "pt-BR";
+// - Função para falar.
+function falar() {
+	speechSynthesis.speak(utterance);
+}
+// - Função para parar de falar.
+function parar() {
+  speechSynthesis.cancel();
+}
+// - Função para definir o texto a ser falado.
+function textoParaFalar() {
+    utterance.text = $("input").val();
+    falar()
+}
+// - Quando o botão de falar for acionado.
+$('.btn__falar').click(() => { 
+    textoParaFalar()
+});
+// - Quando o botão de parar de falar for acionado.
+$('.btn__parar').click(() => {
+    parar()
+})
